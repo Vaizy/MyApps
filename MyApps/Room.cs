676b00cf37fcc3f6be7e9600924e9da80d6448f0
Ivan Ilin <vanilin.va@yandex.ru>
2025-10-19 20:44:30 +0300

    add Room struct

---
+++ MyApps/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApps
+{
+    internal struct Room
+    {
+        public Unit Unit;
+        public Weapon Weapon;
+
+        public Room(Unit unit, Weapon weapon)
+        {
+            Unit = unit;
+            Weapon = weapon;
+        }
+    }
+}
