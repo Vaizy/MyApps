4cf90e505818e1608042fefdfd998382f2f4cb00
Ivan Ilin <vanilin.va@yandex.ru>
2025-11-03 20:41:58 +0300

    add strings

--- MyApps/Program.cs
+++ MyApps/Program.cs
@@ -8,44 +8,67 @@ namespace MyApps
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.Write("Input first operand: ");
-            if (!Int32.TryParse(Console.ReadLine(), out var firstOperand))
-            {
-                Console.WriteLine("Not a number!");
-                return;
-            }
-
-            Console.Write("Input second operand: ");
-            if (!Int32.TryParse(Console.ReadLine(), out var secondOperand))
-            {
-                Console.WriteLine("Not a number!");
-                return;
-            }
-
-            Console.Write("Input operator (&, | or ^): ");
-            var @operator = Console.ReadLine();
-            var result = 0;
-            switch (@operator)
-            {
-                case "&":
-                    result = firstOperand & secondOperand;
-                    break;
-                case "|":
-                    result = firstOperand | secondOperand;
-                    break;
-                case "^":
-                    result = firstOperand ^ secondOperand;
-                    break;
-                default:
-                    Console.WriteLine("Wrong operator");
-                    return;
-            }
-
-            Console.WriteLine("Result in 2: {0}", Convert.ToString(result, 2));
-            Console.WriteLine("Result in 10: {0}", Convert.ToString(result, 10));
-            Console.WriteLine("Result in 16: {0}", Convert.ToString(result, 16));
+            Console.WriteLine(ConcatenateStrings("first", "player"));
+            Console.WriteLine();
+            Console.WriteLine(GreetUser("Ivan", 31));
+            Console.WriteLine();
+            Console.WriteLine(GetInfo("Lorem ipsum dolor sit amet, consectetur adipiscing elit, Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+            Console.WriteLine();
+            Console.WriteLine(GetSubstring("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+            Console.WriteLine(GetSubstring("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+            Console.WriteLine();
+            Console.WriteLine(MakeSentence(new string[] { "Lorem", "ipsum", "dolor", "sit" }));
+            Console.WriteLine();
+            Console.WriteLine(ReplaceWords("Hello world world", "world", "universe"));
         }
+
+        // Задание 1
+        public static string ConcatenateStrings(string str1, string str2)
+        {
+            var builder = new StringBuilder();
+            builder.Append(str1);
+            builder.Append(str2);
+            return builder.ToString();
+        }
+
+        // Задание 2
+        public static string GreetUser(string name, int age)
+        {
+            return $"Hello, {name}!\nYou are {age} years old.";
+        }
+
+        // Задание 3
+        public static string GetInfo(string str)
+        {
+            return $"Количество символов {str.Length}\nСтрока в верхнем регистре {str.ToUpper()}\nСтрока в нижнем регистре {str.ToLower()}";
+        }
+
+        // Задание 4
+        public static string GetSubstring(string str)
+        {
+            return str.Substring(0, 5);
+        }
+
+        // Задание 5
+        public static string MakeSentence(string[] arrayStr)
+        {
+            var builder = new StringBuilder();
+            foreach(string str in arrayStr)
+            {
+                builder.Append(str);
+                if (arrayStr.Last() != str) builder.Append(" ");
+            }
+            return builder.ToString();
+        }
+
+        // Задание 6
+        public static string ReplaceWords(string inputString, string wordToReplace, string replacementWord)
+        {
+            return inputString.Replace(wordToReplace, replacementWord);
+        }
+
     }
 }
